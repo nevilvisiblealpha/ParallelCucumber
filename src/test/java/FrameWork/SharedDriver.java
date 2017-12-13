626b505e6a7abd9f1268fe31554e081bc05988df
nevil.panchal <nevil.panchal@visiblealpha.com>
2017-12-13 15:25:52 +0530

    sharedDriver with absolute path

--- src/test/java/FrameWork/SharedDriver.java
+++ src/test/java/FrameWork/SharedDriver.java
@@ -24,6 +24,7 @@ public class SharedDriver {
     public void init() throws Exception {
         if (!startBrowser) {
 
+
             File file = new File("src\\test\\resources\\chromedriver.exe");
             System.setProperty("webdriver.chrome.driver",file.getAbsolutePath());
             driver = new ChromeDriver();
