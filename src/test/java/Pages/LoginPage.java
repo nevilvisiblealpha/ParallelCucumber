ddb317612139ff360e6f49da6b51784fffa4a78d
nevil.panchal <nevil.panchal@visiblealpha.com>
2017-12-15 13:01:08 +0530

    deleted unwanted file

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -4,16 +4,24 @@ import FrameWork.SharedDriver;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
     public WebDriver driver;
     public LoginPage(SharedDriver sharedDriver) {
         this.driver = sharedDriver.getDriver();
-      //  PageFactory.initElements(driver, this);
+       PageFactory.initElements(driver, this);
     }
+    @FindBy(how= How.ID, using="user_email") WebElement EmailTextbox;
+    @FindBy(how=How.ID, using="user_password") WebElement PassWordTexBox;
+    @FindBy(how=How.XPATH, using="//input[@type='submit']") WebElement SubmitButton;
+    @FindBy(how=How.XPATH,using="//a[@class='profile dropdown-toggle']//span[@class='visible-lg-inline'][text()='?']")WebElement LoggedInName;
 
 
-   public void  load(){
+    public void  load(){
 
         driver.get("http://www.mailinator.com");
     }
@@ -25,9 +33,11 @@ public class LoginPage {
 
     public void loginWithUserNameAndPassword(String username,String Password)
     {
-        driver.findElement(By.id("user_email")).sendKeys(username);
-        driver.findElement(By.id("user_password")).sendKeys(Password);
-        driver.findElement(By.xpath("//input[@type='submit']")).submit();
+        EmailTextbox.sendKeys(username);
+        PassWordTexBox.sendKeys(Password);
+        SubmitButton.click();
+       // driver.findElement(By.id("user_password")).sendKeys(Password);
+      //  driver.findElement(By.xpath("//input[@type='submit']")).submit();
     }
 
 
--- src/test/java/StepFiles/LoginSteps.java
+++ src/test/java/StepFiles/LoginSteps.java
@@ -2,22 +2,15 @@ package StepFiles;
 
 
 import Pages.LoginPage;
-import Pages.LoginPage2;
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-
-import javax.xml.ws.handler.LogicalHandler;
-
 public class LoginSteps {
     public LoginPage loginPage;
-    public LoginPage2 loginpage2;
-    public LoginSteps(LoginPage loginPageinit, LoginPage2 loginpage2)
+    public LoginSteps(LoginPage loginPageinit)
     {
         this.loginPage = loginPageinit;
-        this.loginpage2 = loginpage2;
     }
 
     @Given("^I go to the Login Page$")
@@ -48,18 +41,8 @@ loginPage.isErrorMessageDisplayed(ErrorMessage);
     }
 
 
-    @When("^I enter \"(.*?)\" email address$")
-    public void i_enter_email_address(String emailid) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        loginpage2.EnterEmailAddress(emailid);
 
 
-    }
-    @Then("^verify page is loaded for me$")
-    public void verify_page_is_loaded_for_me() throws Throwable {
-       //c
-        System.out.println("OK tested");
-    }
 
 
     @Then("^I should be able to see logged in user as \"([^\"]*)\"$")
